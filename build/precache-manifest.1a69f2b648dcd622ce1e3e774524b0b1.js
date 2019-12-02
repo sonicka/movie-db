ee7dfac67384fe1b4fed7ae7323c09a1ad0c6cb8
Soňa Leitmanová <sona.leitmanova@gmail.com>
2019-12-02 13:47:42 +0100

    firebase added

---
+++ build/precache-manifest.1a69f2b648dcd622ce1e3e774524b0b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55df0d8b4b655aad36f594181efab7a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f63500b0f5a1c4eba18",
+    "url": "/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "21b306d9a80010829f10",
+    "url": "/static/css/main.fc9a5e8c.chunk.css"
+  },
+  {
+    "revision": "0f63500b0f5a1c4eba18",
+    "url": "/static/js/2.76ca2c28.chunk.js"
+  },
+  {
+    "revision": "21b306d9a80010829f10",
+    "url": "/static/js/main.db479f6b.chunk.js"
+  },
+  {
+    "revision": "5621b446f92130edaa43",
+    "url": "/static/js/runtime-main.34444482.js"
+  }
+]);
